ac23341644c23b00e5fa2dfdccee917977636233
facundo <facundoguerra12@gmail.com>
2022-12-05 17:33:37 -0300

    trabajo terminado

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import SideBar from './SideBar';
+import {Route, Switch} from 'react-router-dom';
+import ContentWrapper from './ContentWrapper';
+import LastMovieInDb from './LastMovieInDb';
+import GenresInDb from './GenresInDb';
+import ContentRowMovies from './ContentRowMovies';
+import NotFound from './NotFound';
+function App() {
+    return (
+        <React.Fragment>
+            <div id='wrapper'>
+                <SideBar />
+                <Switch>
+                    <Route path='/' exact>
+                        <ContentWrapper />
+                    </Route>
+                    <Route path='/LastMovieInDb' component={LastMovieInDb} />
+                    <Route path='/GenresInDb' component={GenresInDb} />
+                    <Route path='/ContentRowMovies' component={ContentRowMovies} />
+                    <Route component={NotFound} />
+                </Switch>
+            </div>
+        </React.Fragment>
+           /*Micro desafío - Paso 2:*/
+    );
+}
+
+export default App;
